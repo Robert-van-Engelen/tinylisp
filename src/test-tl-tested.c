675f9eee4a348391f2434c4fb1ed44e7a4a9c03b
Pierre Jacomet <Pierre.Jacomet@hotmail.com>
2023-01-04 18:33:59 -0800

    Making tests more data driven

--- src/test-tl-tested.c
+++ src/test-tl-tested.c
@@ -149,28 +149,37 @@ static void test_scan_quotes_in_symbol(void) {
   TEST_ASSERT_EQUAL_STRING_MESSAGE(expected, out_buf, "Difference in strings");
 }
 
-static void test_scan_del_spaces_before_token(void) {
-  // clang-format off
-  const char *expectations[] = {
-      "a",
-      "b",
-      "(", 
-      "\"", 
-      "'", 
-      "ao%vq\"'#", 
-      "define", 
-      "......", 
-      "another_one_bites_the_dust",
-      "________%%%__________",
-      "______'''''@#$@%^#*"
+typedef struct {
+  const char *in;
+  const char *out;
+  const char *msg;
+} expectation;
+
+static void test_scanner(void) {
+  // c\\lang-format off
+
+  const expectation expectations[] = {
+      {"a",                          NULL,       "letter"     },
+      {"a      (",                   "a",        "letter"     },
+      {"b",                          NULL,       "letter"     },
+      {"(",                          NULL,       "Single LP"  },
+      {"(        ",                  "(",        "Single LP"  },
+      {"\"",                         NULL,       "token"      },
+      {"'",                          NULL,       "msg"        },
+      {"ao%vq\"'(#",                 "ao%vq\"'", "Weird token"},
+      {"define",                     NULL,       "msg"        },
+      {"......",                     NULL,       "msg"        },
+      {"another_one_bites_the_dust", NULL,       "msg"        },
+      {"________%%%__________",      NULL,       "msg"        },
+      {"______'''''@#$@%^#*",        NULL,       "msg"        }
   };
-  // clang-format on
-  for (int i = 0; i < (sizeof(expectations) / sizeof(char *)); i++) {
-    const char *expected = expectations[i];
+  // c\\lang-format on
+  for (int i = 0; i < (sizeof(expectations) / sizeof(expectation)); i++) {
+    const expectation exp = expectations[i];
     char in_buf[1024];
-    sprintf(in_buf, "               %s", expected);
+    sprintf(in_buf, "               %s", exp.in);
     const char *out_buf = exercise_scan(in_buf);
-    TEST_ASSERT_EQUAL_STRING(expected, out_buf);
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(exp.out ? exp.out : exp.in, out_buf, exp.msg);
   }
 }
 
@@ -188,7 +197,7 @@ int main(void) {
   RUN_TEST(test_scan_symbol_string);
   RUN_TEST(test_scan_single_quote);
   RUN_TEST(test_scan_quotes_in_symbol);
-  RUN_TEST(test_scan_del_spaces_before_token);
+  RUN_TEST(test_scanner);
   // RUN_TEST(test_single_number);
   // RUN_TEST(test_simple_addition);
   // RUN_TEST(test_several_expressions);
