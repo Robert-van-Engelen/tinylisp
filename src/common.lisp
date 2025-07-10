7a2b3bf5eb4ac35f2801d312da6aa7d8cc6d0ae4
Robert van Engelen <88845949+Robert-van-Engelen@users.noreply.github.com>
2025-07-10 10:32:55 -0400

    Update common.lisp

--- src/common.lisp
+++ src/common.lisp
@@ -1,7 +1,6 @@
 (define null? not)
 (define err? (lambda (x) (eq? x 'ERR)))
 (define number? (lambda (x) (eq? (* 0 x) 0)))
-(define pair? (lambda (x) (not (err? (cdr x)))))
 (define symbol?
     (lambda (x)
         (and
