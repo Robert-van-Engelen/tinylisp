367f20e470ad341ab192e0e140ef758ee3e7ec32
Robert van Engelen <88845949+Robert-van-Engelen@users.noreply.github.com>
2025-07-12 16:48:02 -0400

    add unit test for catch & throw

--- tests/dotcall-extras.lisp
+++ tests/dotcall-extras.lisp
@@ -1,157 +1,158 @@
 (cons
- (if (equal?
-      ((lambda (x y z) (list x y z)) '(1) '(2) '(3)) '((1) (2) (3)))
-     'passed
-     'failed)
- '(same-args))
+    (if (equal?
+            ((lambda (x y z) (list x y z)) '(1) '(2) '(3))
+            '((1) (2) (3)))
+        'passed
+        'failed)
+    '(same-args))
 
 (cons
- (if (equal?
-      ((lambda (x y) (list x y)) '(1) '(2) '(3)) '((1) (2)))
-     'passed
-     'failed)
- '(extra-args))
+    (if (equal?
+            ((lambda (x y) (list x y)) '(1) '(2) '(3))
+            '((1) (2)))
+        'passed
+        'failed)
+    '(extra-args))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         ((lambda (x y z) (list x y z)) '(1) '(2) . l)) '((3)))
-      '((1) (2) (3)))
-     'passed
-     'failed)
- '(caller-dot))
+    (if (equal?
+            ((lambda (l)
+                ((lambda (x y z) (list x y z)) '(1) '(2) . l)) '((3)))
+            '((1) (2) (3)))
+        'passed
+        'failed)
+    '(caller-dot))
 
 (cons
- (if (equal?
-      ((lambda (x y . z) (list x y z)) '(1) '(2) '(3))
-      '((1) (2) ((3))))
-     'passed
-     'failed)
- '(callee-dot))
+    (if (equal?
+            ((lambda (x y . z) (list x y z)) '(1) '(2) '(3))
+            '((1) (2) ((3))))
+        'passed
+        'failed)
+    '(callee-dot))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         ((lambda (x y . z) (list x y z)) '(1) '(2) . l)) '((3)))
-      '((1) (2) ((3))))
-     'passed
-     'failed)
- '(both-dot))
+    (if (equal?
+            ((lambda (l)
+                ((lambda (x y . z) (list x y z)) '(1) '(2) . l)) '((3)))
+            '((1) (2) ((3))))
+        'passed
+        'failed)
+    '(both-dot))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         ((lambda (x . y) (list x y)) '(1) '(2) . l)) '((3)))
-      '((1) ((2) (3))))
-     'passed
-     'failed)
- '(extra-dot))
+    (if (equal?
+            ((lambda (l)
+                ((lambda (x . y) (list x y)) '(1) '(2) . l)) '((3)))
+            '((1) ((2) (3))))
+        'passed
+        'failed)
+    '(extra-dot))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         ((lambda (x . y) (list x y)) '(1) . l)) '((2) (3)))
-      '((1) ((2) (3))))
-     'passed
-     'failed)
- '(scant-dot))
+    (if (equal?
+            ((lambda (l)
+                ((lambda (x . y) (list x y)) '(1) . l)) '((2) (3)))
+            '((1) ((2) (3))))
+        'passed
+        'failed)
+    '(scant-dot))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         ((lambda (x y . z) (list x y z)) '(1) . l)) '((2) (3)))
-      '((1) (2) ((3))))
-     'passed
-     'failed)
- '(early-dot))
+    (if (equal?
+            ((lambda (l)
+                ((lambda (x y . z) (list x y z)) '(1) . l)) '((2) (3)))
+            '((1) (2) ((3))))
+        'passed
+        'failed)
+    '(early-dot))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         (+ . l)) '(1 2 3))
-      6)
-     'passed
-     'failed)
- '(builtin-add))
+    (if (equal?
+            ((lambda (l) (+ . l)) '(1 2 3))
+            6)
+        'passed
+        'failed)
+    '(builtin-add))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         (- . l)) '(1 2 3))
-      -4)
-     'passed
-     'failed)
- '(builtin-sub))
+    (if (equal?
+            ((lambda (l) (- . l)) '(1 2 3))
+            -4)
+        'passed
+        'failed)
+    '(builtin-sub))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         (* . l)) '(1 2 3))
-      6)
-     'passed
-     'failed)
- '(builtin-mul))
+    (if (equal?
+            ((lambda (l) (* . l)) '(1 2 3))
+            6)
+        'passed
+        'failed)
+    '(builtin-mul))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         (/ . l)) '(1 2))
-      0.5)
-     'passed
-     'failed)
- '(builtin-div))
+    (if (equal?
+            ((lambda (l) (/ . l)) '(1 2))
+            0.5)
+        'passed
+        'failed)
+    '(builtin-div))
 
 (cons
- (if (eq?
-      (let* (x 1) (y (+ 1 x))
-            (let* (z (+ x y))
-                  z))
-      3)
-      'passed
-      'failed)
-  '(let*)
-)
+    (if (equal?
+            (let*
+                (x 1) (y (+ 1 x))
+                (let* (z (+ x y)) z))
+            3)
+        'passed
+        'failed)
+    '(let*))
 
 (cons
- (if (eq?
-      (let (x 1) (y 2)
-           (let (z (+ x y))
-                z))
-      3)
-      'passed
-      'failed)
-  '(let)
-)
+    (if (equal?
+            (let (x 1) (y 2)
+              (let (z (+ x y)) z))
+            3)
+        'passed
+        'failed)
+    '(let))
 
 (cons
- (if (eq?
-      (let* (x 5)
-            (letrec* (f (lambda (n) (if (< n 2) 1 (* n (f (- n 1))))))
-              (f x)))
-      120)
-      'passed
-      'failed)
-  '(letrec*)
-)
+    (if (equal?
+            (let* (x 5)
+                (letrec* (f (lambda (n) (if (< n 2) 1 (* n (f (- n 1)))))) (f x)))
+            120)
+        'passed
+        'failed)
+    '(letrec*))
 
 (cons
- (if (eq?
-      (let* (delay (macro (x) (list 'lambda () x))) (f (delay (+ 1 2)))
-            (f))
-      3)
-      'passed
-      'failed)
-  '(macro)
-)
+    (if (equal?
+            (let* (delay (macro (x) (list 'lambda () x))) (f (delay (+ 1 2))) (f))
+            3)
+        'passed
+        'failed)
+    '(macro))
 
 (cons
- (if (eq?
-      (let* (a 1) (b 2) (e (list (list 'x . a) (list 'y . b)))
-            (+ (assoc 'x e) (assoc 'y e)))
-      3)
-      'passed
-      'failed)
-  '(assoc)
-)
+    (if (equal?
+            (let*
+                (a 1) (b 2) (e (list (list 'x . a) (list 'y . b)))
+                (+ (assoc 'x e) (assoc 'y e)))
+            3)
+        'passed
+        'failed)
+    '(assoc))
+
+(cons
+    (if (equal?
+            (letrec*
+                (f (lambda (n) (if (eq? n 9) (throw 9) (f (+ n 1)))))
+                (catch (f 1)))
+            '(ERR . 9))
+        'passed
+        'failed)
+    '(catch-throw))
 
 'OK
--- tests/dotcall.lisp
+++ tests/dotcall.lisp
@@ -7,116 +7,116 @@
                 (pair? y)
                 (equal? (car x) (car y))
                 (equal? (cdr x) (cdr y))))))
+
 (define list (lambda args args))
 
 (cons
- (if (equal?
-      ((lambda (x y z) (list x y z)) '(1) '(2) '(3)) '((1) (2) (3)))
-     'passed
-     'failed)
- '(same-args))
+    (if (equal?
+            ((lambda (x y z) (list x y z)) '(1) '(2) '(3))
+            '((1) (2) (3)))
+        'passed
+        'failed)
+    '(same-args))
 
 (cons
- (if (equal?
-      ((lambda (x y) (list x y)) '(1) '(2) '(3)) '((1) (2)))
-     'passed
-     'failed)
- '(extra-args))
+    (if (equal?
+            ((lambda (x y) (list x y)) '(1) '(2) '(3))
+            '((1) (2)))
+        'passed
+        'failed)
+    '(extra-args))
 
 (cons
- (if (equal?
-      ((lambda (x y z) (list x y z)) '(1) '(2)) '((1) (2) ERR))
-     'passed
-     'failed)
- '(scant-args))
+    (if (equal?
+            ((lambda (x y z) (list x y z)) '(1) '(2))
+            '((1) (2) ERR))
+        'passed
+        'failed)
+    '(scant-args))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         ((lambda (x y z) (list x y z)) '(1) '(2) . l)) '((3)))
-      '((1) (2) (3)))
-     'passed
-     'failed)
- '(caller-dot))
+    (if (equal?
+            ((lambda (l)
+                ((lambda (x y z) (list x y z)) '(1) '(2) . l)) '((3)))
+            '((1) (2) (3)))
+        'passed
+        'failed)
+    '(caller-dot))
 
 (cons
- (if (equal?
-      ((lambda (x y . z) (list x y z)) '(1) '(2) '(3))
-      '((1) (2) ((3))))
-     'passed
-     'failed)
- '(callee-dot))
+    (if (equal?
+            ((lambda (x y . z) (list x y z)) '(1) '(2) '(3))
+            '((1) (2) ((3))))
+        'passed
+        'failed)
+    '(callee-dot))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         ((lambda (x y . z) (list x y z)) '(1) '(2) . l)) '((3)))
-      '((1) (2) ((3))))
-     'passed
-     'failed)
- '(both-dot))
+    (if (equal?
+            ((lambda (l)
+                ((lambda (x y . z) (list x y z)) '(1) '(2) . l)) '((3)))
+            '((1) (2) ((3))))
+        'passed
+        'failed)
+    '(both-dot))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         ((lambda (x . y) (list x y)) '(1) '(2) . l)) '((3)))
-      '((1) ((2) (3))))
-     'passed
-     'failed)
- '(extra-dot))
+    (if (equal?
+            ((lambda (l)
+                ((lambda (x . y) (list x y)) '(1) '(2) . l)) '((3)))
+            '((1) ((2) (3))))
+        'passed
+        'failed)
+    '(extra-dot))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         ((lambda (x . y) (list x y)) '(1) . l)) '((2) (3)))
-      '((1) ((2) (3))))
-     'passed
-     'failed)
- '(scant-dot))
+    (if (equal?
+            ((lambda (l)
+                ((lambda (x . y) (list x y)) '(1) . l)) '((2) (3)))
+            '((1) ((2) (3))))
+        'passed
+        'failed)
+    '(scant-dot))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         ((lambda (x y . z) (list x y z)) '(1) . l)) '((2) (3)))
-      '((1) (2) ((3))))
-     'passed
-     'failed)
- '(early-dot))
+    (if (equal?
+            ((lambda (l)
+                ((lambda (x y . z) (list x y z)) '(1) . l)) '((2) (3)))
+            '((1) (2) ((3))))
+        'passed
+        'failed)
+    '(early-dot))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         (+ . l)) '(1 2 3))
-      6)
-     'passed
-     'failed)
- '(builtin-add))
+    (if (equal?
+            ((lambda (l) (+ . l)) '(1 2 3))
+            6)
+        'passed
+        'failed)
+    '(builtin-add))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         (- . l)) '(1 2 3))
-      -4)
-     'passed
-     'failed)
- '(builtin-sub))
+    (if (equal?
+            ((lambda (l) (- . l)) '(1 2 3))
+            -4)
+        'passed
+        'failed)
+    '(builtin-sub))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         (* . l)) '(1 2 3))
-      6)
-     'passed
-     'failed)
- '(builtin-mul))
+    (if (equal?
+            ((lambda (l) (* . l)) '(1 2 3))
+            6)
+        'passed
+        'failed)
+    '(builtin-mul))
 
 (cons
- (if (equal?
-      ((lambda (l)
-         (/ . l)) '(1 2))
-      0.5)
-     'passed
-     'failed)
- '(builtin-div))
+    (if (equal?
+            ((lambda (l) (/ . l)) '(1 2))
+            0.5)
+        'passed
+        'failed)
+    '(builtin-div))
 
 'OK
