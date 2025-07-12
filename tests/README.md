3fab42985fc48c311439dcf725f25212fc63d703
Robert van Engelen <88845949+Robert-van-Engelen@users.noreply.github.com>
2025-07-12 12:31:18 -0400

    add unit tests for tinylisp-extras.c

--- tests/README.md
+++ tests/README.md
@@ -1,6 +1,6 @@
 # Unit tests
 
-The dot operator in lambda variable lists and in actual argument lists is tested for all combinations.  Arithmetic and logic is tested.  List operations are tested.
+The dot operator in lambda variable lists and in actual argument lists is tested for all combinationsin [dotcall.lisp](dotcall.lisp), arithmetic and logic is tested, and list operations are tested:
 
 ```console
 $ ./tinylisp < dotcall.lisp
@@ -23,4 +23,31 @@ tinylisp
 800>OK
 ```
 
-**Note**: when section 14 "error handlign and exceptions" is implemented, then the third test case fails as expected because the missing actual argument raises an exception.
+When the article's additional Lisp features are implemented, then test with [dotcall-extras.lisp](dotcall-extras.lisp):
+
+```console
+$ ./tinylisp-extras
+tinylisp
+...
+7431>(load dotcall-extras.lisp)
+dotcall-extras.lisp
+(passed same-args)
+(passed extra-args)
+(passed caller-dot)
+(passed callee-dot)
+(passed both-dot)
+(passed extra-dot)
+(passed scant-dot)
+(passed early-dot)
+(passed builtin-add)
+(passed builtin-sub)
+(passed builtin-mul)
+(passed builtin-div)
+(passed let*)
+(passed let)
+(passed letrec*)
+(passed macro)
+(passed assoc)
+OK
+```
+
---
+++ tests/dotcall-extras.lisp
@@ -0,0 +1,157 @@
+(cons
+ (if (equal?
+      ((lambda (x y z) (list x y z)) '(1) '(2) '(3)) '((1) (2) (3)))
+     'passed
+     'failed)
+ '(same-args))
+
+(cons
+ (if (equal?
+      ((lambda (x y) (list x y)) '(1) '(2) '(3)) '((1) (2)))
+     'passed
+     'failed)
+ '(extra-args))
+
+(cons
+ (if (equal?
+      ((lambda (l)
+         ((lambda (x y z) (list x y z)) '(1) '(2) . l)) '((3)))
+      '((1) (2) (3)))
+     'passed
+     'failed)
+ '(caller-dot))
+
+(cons
+ (if (equal?
+      ((lambda (x y . z) (list x y z)) '(1) '(2) '(3))
+      '((1) (2) ((3))))
+     'passed
+     'failed)
+ '(callee-dot))
+
+(cons
+ (if (equal?
+      ((lambda (l)
+         ((lambda (x y . z) (list x y z)) '(1) '(2) . l)) '((3)))
+      '((1) (2) ((3))))
+     'passed
+     'failed)
+ '(both-dot))
+
+(cons
+ (if (equal?
+      ((lambda (l)
+         ((lambda (x . y) (list x y)) '(1) '(2) . l)) '((3)))
+      '((1) ((2) (3))))
+     'passed
+     'failed)
+ '(extra-dot))
+
+(cons
+ (if (equal?
+      ((lambda (l)
+         ((lambda (x . y) (list x y)) '(1) . l)) '((2) (3)))
+      '((1) ((2) (3))))
+     'passed
+     'failed)
+ '(scant-dot))
+
+(cons
+ (if (equal?
+      ((lambda (l)
+         ((lambda (x y . z) (list x y z)) '(1) . l)) '((2) (3)))
+      '((1) (2) ((3))))
+     'passed
+     'failed)
+ '(early-dot))
+
+(cons
+ (if (equal?
+      ((lambda (l)
+         (+ . l)) '(1 2 3))
+      6)
+     'passed
+     'failed)
+ '(builtin-add))
+
+(cons
+ (if (equal?
+      ((lambda (l)
+         (- . l)) '(1 2 3))
+      -4)
+     'passed
+     'failed)
+ '(builtin-sub))
+
+(cons
+ (if (equal?
+      ((lambda (l)
+         (* . l)) '(1 2 3))
+      6)
+     'passed
+     'failed)
+ '(builtin-mul))
+
+(cons
+ (if (equal?
+      ((lambda (l)
+         (/ . l)) '(1 2))
+      0.5)
+     'passed
+     'failed)
+ '(builtin-div))
+
+(cons
+ (if (eq?
+      (let* (x 1) (y (+ 1 x))
+            (let* (z (+ x y))
+                  z))
+      3)
+      'passed
+      'failed)
+  '(let*)
+)
+
+(cons
+ (if (eq?
+      (let (x 1) (y 2)
+           (let (z (+ x y))
+                z))
+      3)
+      'passed
+      'failed)
+  '(let)
+)
+
+(cons
+ (if (eq?
+      (let* (x 5)
+            (letrec* (f (lambda (n) (if (< n 2) 1 (* n (f (- n 1))))))
+              (f x)))
+      120)
+      'passed
+      'failed)
+  '(letrec*)
+)
+
+(cons
+ (if (eq?
+      (let* (delay (macro (x) (list 'lambda () x))) (f (delay (+ 1 2)))
+            (f))
+      3)
+      'passed
+      'failed)
+  '(macro)
+)
+
+(cons
+ (if (eq?
+      (let* (a 1) (b 2) (e (list (list 'x . a) (list 'y . b)))
+            (+ (assoc 'x e) (assoc 'y e)))
+      3)
+      'passed
+      'failed)
+  '(assoc)
+)
+
+'OK
