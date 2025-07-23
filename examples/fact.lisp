4b5b909e605650af818e984089138176282a85c8
Robert van Engelen <88845949+Robert-van-Engelen@users.noreply.github.com>
2025-07-23 08:58:54 -0400

    add a README

--- examples/fact.lisp
+++ examples/fact.lisp
@@ -8,6 +8,17 @@
                      (f (* r k) (- k 1)))))
              (f 1 n))))
 
+; using the Y combinator for recursion without letrec* recursive naming
+(define Y (lambda (f) (lambda args ((f (Y f)) . args))))
+(define facty
+    (lambda (n)
+        ((Y (lambda (f)
+                (lambda (k)
+                    (if (< 1 k)
+                        (* k (f (- k 1)))
+                        1))))
+         n)))
+
 ; standard recursive factorial function is not tail-recursive
 (define dumbfact
     (lambda (n)
