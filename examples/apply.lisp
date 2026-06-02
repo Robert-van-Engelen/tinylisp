296393b83a0b9cbefba484a96519807c71c3c11d
Robert van Engelen <88845949+Robert-van-Engelen@users.noreply.github.com>
2026-06-02 14:50:13 +0200

    Update apply.lisp

--- examples/apply.lisp
+++ examples/apply.lisp
@@ -13,7 +13,7 @@
 ; When <list-expr> is an expression then we use a let* to bind <var> first:
 ; (let* (args <list-expr>) (<func> <expr1> <expr2> ... <exprk> . args))
 
-; We can also define a macro to do all this construction work automatically for you:
+; We can also define a macro to do all this construction automatically for you:
 
 (define apply
     (macro fargs
@@ -27,3 +27,18 @@
                     (cons (car t) (app (cdr t) s))
                     s)))
             (list 'let* (cons '_ (last fargs)) (app fargs '_)))))
+
+; Alternatively, using macro backquoting:
+
+; (define apply
+;     (macro fargs
+;         (letrec*
+;             (last (lambda (t)
+;                 (if (cdr t)
+;                     (last (cdr t))
+;                     t)))
+;             (app (lambda (t s)
+;                 (if (cdr t)
+;                     (cons (car t) (app (cdr t) s))
+;                     s)))
+;             `(let* (_ ,(car (last fargs))) ,(app fargs '_)))))
