de641040689d968a3b5c7698869296f753cb0fd6
Robert van Engelen <88845949+Robert-van-Engelen@users.noreply.github.com>
2026-07-23 12:10:21 +0200

    Update atomize.lisp

--- GC/atomize.lisp
+++ GC/atomize.lisp
@@ -1,16 +1,17 @@
 ; example atomizations for tinylisp-extras-gc with new atomize primitive
 
 ; atomization rules:
-;   atomize atom        => atom
-;   atomize number      => atom with number in decimal text
-;   atomize 'atom       => atom
-;   atomize `atom       => atom
-;   atomize "text"      => atom with given text that may contain spacing
-;   atomize ()          => atomize " "
-;   atomize x y ...     => concatenate atomize x atomize y atomize ...
-;   atomize '(x y ...)  => concatenate atomize x atomize y atomize ...
-;   atomize `(x y ...)  => concatenate atomize x atomize y atomize ...
-;   atomize (f x y ...) => atomize the result of the evaluation of (f x y ...)
+;   atomize atom         => atom
+;   atomize number       => atom with number in decimal text
+;   atomize 'atom        => atom
+;   atomize `atom        => atom
+;   atomize "text"       => atom with given text that may contain spacing
+;   atomize ()           => atomize " "
+;   atomize x y ...      => concatenate atomize x atomize y atomize ...
+;   atomize '(x y ...)   => concatenate atomize x atomize y atomize ...
+;   atomize `(x y ...)   => concatenate atomize x atomize y atomize ...
+;   atomize (f x y ...)  => atomize the result of the evaluation of (f x y ...)
+;   atomize (progn atom) => atomize the value of the variable named atom
 ;
 ; atomize is used by:
 ;   (load pathname)          atomizes pathname of the file to open and load
